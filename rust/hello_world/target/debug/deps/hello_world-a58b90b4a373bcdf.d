bf36700014fe6e92146227e26dfc349af6731cc6
asdfstudio <sss.joy.one@gmail.com>
2024-08-21 00:16:19 +0600

    init

---
+++ rust/hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf.d
@@ -0,0 +1,5 @@
+/home/ki/Desktop/dev/rust/hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf: src/main.rs
+
+/home/ki/Desktop/dev/rust/hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf.d: src/main.rs
+
+src/main.rs:
